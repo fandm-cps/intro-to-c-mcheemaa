cb6087d232770a0427ef13648d0b61414d468986
mcheema <mcheema@cs-122>
2019-09-02 14:48:00 -0400

    New File Guess Game

---
+++ exercise1.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+int main()
+{
+
+  using namespace std;
+  int x = 0;
+  int y = 6;
+
+  cout << endl;
+  cout << x - y << " " << x * y << " " << x + y;
+
+  cout << endl;
+
+  return 0;
+  
+  
+}
--- hello_world.cpp
+++ hello_world.cpp
@@ -3,27 +3,16 @@
 #include <stdlib.h>
 #include <iostream>
 
+// we can make comments like this 
+
 int main(int argc, char* argv[])
 {
-  if(argc < 2)
-    {
-      std::cout << "Error: " << "./hello_world.exe n" << std::endl;
-      return 1;
-    }
 
-  int n = atoi(argv[1]);
- 
-  std::cout << "C++- Style" << std::endl;
-  for(int i = 0; i < n; i++)
-    {
-      std::cout << "Hello World" << std::endl;
-    }
+  std::cout << "Hello World" << std::endl;
+  printf("Hello World\n");
 
-  printf("C Style \n");
-  for(int i = 0; i < n; i++)
-    {
-      printf("C - Hello World \n");
-    }
   
-  return 0;
+
+
+  
 }
